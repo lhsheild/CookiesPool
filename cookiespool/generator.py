eb35058e1f4c08180004bc22ecc03c8bb7f2ff62
lhsheild <lhsheild@yahoo.com>
2019-11-15 10:25:48 +0800

    [UPDATE] 更新chromedriver为linux环境

--- cookiespool/generator.py
+++ cookiespool/generator.py
@@ -34,14 +34,15 @@ class CookiesGenerator(object):
             self.browser = webdriver.PhantomJS(desired_capabilities=caps)
             self.browser.set_window_size(1400, 500)
         elif BROWSER_TYPE == 'Chrome':
-            chrome_driver = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+            # chrome_driver = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
             options = webdriver.ChromeOptions()
             # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
             options.add_experimental_option('excludeSwitches', ['enable-automation'])
             options.add_argument('--headless')
             # driver = webdriver.Chrome(chromedriver, options=options)
-            self.browser = webdriver.Chrome(executable_path=chrome_driver, options=options)
-    
+            # self.browser = webdriver.Chrome(executable_path=chrome_driver, options=options)
+            self.browser = webdriver.Chrome(options=options)
+
     def new_cookies(self, username, password):
         """
         新生成Cookies，子类需要重写
